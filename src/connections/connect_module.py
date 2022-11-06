bdae7910bd9404cb11d19c17ac0157bd3bf7fa01
Ireal <pesotskygoscha@yandex.ru>
2022-11-06 21:55:34 +0300

    second update settings

--- src/connections/connect_module.py
+++ src/connections/connect_module.py
@@ -1,13 +1,10 @@
 import psycopg2
 from psycopg2 import Error
-from sqlalchemy import select, insert, update, delete
+from sqlalchemy import create_engine, select, insert, update, delete
 from sqlalchemy.orm import declarative_base, sessionmaker
 import os
-from settings import Settings
+from config import settings
 
-settings = Settings()
-
-engine = settings.DATABASE_URL
+engine = create_engine(settings.DATABASE_URL)
 Session = sessionmaker(engine)
 Base = declarative_base()
-
